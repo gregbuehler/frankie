4d6a9e512f7da8060c4aedc42cf721480487ac38
Lucas Hill <kingofallhills@gmail.com>
2015-10-15 18:39:45 -0700

    Adding basic functionality for interacting with geolocation/weather APIs

--- frankie-config.js
+++ frankie-config.js
@@ -1,5 +1,8 @@
 var config = {};
 config.google = {};
-config.google.apiKey = "";
+config.google.apiKey = ""; //Doesn't seem to be necessary?
+
+config.forecastIO = {};
+config.forecastIO.apiKey = ""; //Get from https://developer.forecast.io/
 
 module.exports = config;
